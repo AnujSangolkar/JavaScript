273280ec8deae16d2e75df85db16744108c8a7fd
AnujSangolkar <anujsangolkar1@gmail.com>
2024-01-29 14:12:28 +0530

    practice

---
+++ practice/app.js
@@ -0,0 +1,11 @@
+function scrollToFooter() {
+    document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
+  }
+
+function scrollToAbout(){
+    document.getElementById('about').scrollIntoView({behavior: 'smooth'});
+}
+
+function scrollToHome(){
+    document.getElementById('header').scrollIntoView({behavior:'smooth'});
+}
